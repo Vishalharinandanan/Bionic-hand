7ef1f728c331576a9fd8f5e78249a816e27bf17b
Vishalharinandanan <166580271+Vishalharinandanan@users.noreply.github.com>
2025-04-29 13:26:59 +0530

    Add files via upload

---
+++ flex_and_servo.ino
@@ -0,0 +1,28 @@
+#include <Servo.h> 
+Servo myservo;
+int pos = 0;
+int sensor=A0;
+int X=0;
+void setup() 
+{ 
+ pinMode(A0,INPUT);
+ myservo.attach(9);  
+} 
+void loop()
+{ 
+  X = digitalRead(A0);
+ if(A0==HIGH)
+ {
+  for(pos = 0; pos < 180; pos += 1)  
+  {                                 
+    myservo.write(pos);              
+    delay(15);                      
+  } 
+ }
+ else
+  {for(pos = 180; pos>=1; pos-=1)     
+  {                                
+    myservo.write(pos);              
+    delay(15);          
+  } }
+}
